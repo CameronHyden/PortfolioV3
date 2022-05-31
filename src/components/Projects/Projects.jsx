f3eac7986cdf6e1ae951cc28274c02d413dc969f
CameronHyden <camh.hyden@gmail.com>
2022-05-31 12:23:49 +0100

    set up new project, copied some components from v2

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,30 @@
+import "./Projects.scss";
+import { data } from "../../assets/data/data";
+
+const Projects = ({ isHovering, handleMouseOver, HandleMouseOut }) => {
+  const projectJSX = data.map((d) => {
+    return (
+      <div
+        className="projects"
+        onMouseOver={handleMouseOver}
+        onMouseOut={HandleMouseOut}
+        key={d.id}
+      >
+        <div className="projects__heading">
+          <h2>{d.title}</h2>
+          <a href={d.url}>
+            <img className="projects__icon" src={d.icon} alt="" />
+          </a>
+          <h3>{d.liveSite}</h3>
+        </div>
+        <div className="projects__imageDisplay">
+          <img className="projects__image" src={d.img} alt="" />
+          {isHovering && <p>{d.desc}</p>}
+        </div>
+      </div>
+    );
+  });
+  return projectJSX;
+};
+
+export default Projects;
