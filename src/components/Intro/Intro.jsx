ae15ce2ca6cb273b9a2f8623c700c11db7b44cc6
CameronHyden <camh.hyden@gmail.com>
2022-06-01 11:28:12 +0100

    position changes

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,17 +3,14 @@ import "./Intro.scss";
 const Intro = () => {
   return (
     <div className="intro">
-      <div className="intro__circleContainer">
-        <div className="intro__circle intro__circle--1"></div>
-        <div className="intro__circle intro__circle--2"></div>
-        <div className="intro__circle intro__circle--3"></div>
-        <div className="intro__circle intro__circle--4"></div>
-      </div>
-      <div className="intro__textContainer">
-        <p>
-          Hi there! I am Cameron Hyden. <br />
-          Junior full stack developer
-        </p>
+      <div className="circleContainer">
+        <div className="circleContainer__circle circleContainer__circle--1"></div>
+        <div className="circleContainer__circle circleContainer__circle--2"></div>
+        <div className="circleContainer__circle circleContainer__circle--3"></div>
+        <div className="circleContainer__circle circleContainer__circle--4"></div>
+        <div className="circleContainer__text">
+          <p>Hi there! I am Cameorn Hyden. Juniour full stack developer</p>
+        </div>
       </div>
     </div>
   );
