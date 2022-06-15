a7e201ff9a4c7d0df415a9b379dbd06c2e4bfc88
CameronHyden <camh.hyden@gmail.com>
2022-06-15 19:37:57 +0100

    changes to intro and page size

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,14 +3,12 @@ import "./Intro.scss";
 const Intro = () => {
   return (
     <div className="intro">
-      <div className="circleContainer">
-        <div className="circleContainer__circle circleContainer__circle--1"></div>
-        <div className="circleContainer__circle circleContainer__circle--2"></div>
-        <div className="circleContainer__circle circleContainer__circle--3"></div>
-        <div className="circleContainer__circle circleContainer__circle--4"></div>
-        <div className="circleContainer__text">
-          <p>Hi there! I am Cameron Hyden. Juniour full stack developer</p>
-        </div>
+      <div className="intro__circle intro__circle--1"></div>
+      <div className="intro__circle intro__circle--2"></div>
+      <div className="intro__circle intro__circle--3"></div>
+      <div className="intro__circle intro__circle--4"></div>
+      <div className="intro__text">
+        <p>Hi there! I am Cameron Hyden. Juniour full stack developer</p>
       </div>
     </div>
   );
