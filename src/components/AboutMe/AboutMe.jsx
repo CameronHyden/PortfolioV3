84a268930cde9b2b2db06adfded164627afce4d5
CameronHyden <camh.hyden@gmail.com>
2022-06-20 20:36:42 +0100

    updated about me page

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import "./AboutMe.scss";
 import Me from "../../assets/images/me.png";
-import speech from "../../assets/images/speech.png";
 
 const AboutMe = () => {
   return (
-    <div>
-      <div>
-        <img src={Me} alt="" />
-        <img src={speech} alt="" />
-        <p> SOME TEXT ABOUT ME</p>
-      </div>
+    <div className="aboutMe">
+      <img className="aboutMe__image" src={Me} alt="" />
+      <p className="aboutMe__text">
+        Throughout university and previous job roles, I have always gravitated
+        towards, Innovation, technology and problem solving. From the start of
+        this year I have been working tirelessly to become a valuable software
+        developer, accomplished through a 12 week intensive course and my own
+        personal projects.
+      </p>
     </div>
   );
 };
